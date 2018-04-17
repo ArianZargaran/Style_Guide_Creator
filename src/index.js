5915693f623430ce71d1faa06d301b7c75d79c49
Arian Zargaran <arianzf@gmail.com>
2018-04-16 23:52:44 -0700

    Need to re-organize styles

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import Router from "./components/Router";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/neo.css";
 
+import "./style/Index.css";
 import "./style/sgcreator-hook.css";
 import "./style/sgcreator-categories.css";
 import "./style/sgcreator-section.css";
 import "./style/sgcreator-css.css";
 import "./style/sgcreator-tag-box.css";
-import "./style/Index.css";
 
 ReactDOM.render(
   <Provider store={store}>
