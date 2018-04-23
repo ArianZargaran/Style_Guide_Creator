86deafd9a7401494fb669c2273400ff25b98b5a0
Arian Zargaran <arianzf@gmail.com>
2018-04-22 23:24:29 -0700

    Continue with secitons

--- src/containers/Categories.js
+++ src/containers/Categories.js
@@ -6,6 +6,7 @@ import Download from "../components/Download";
 class Categories extends Component {
   constructor(props) {
     super(props);
+
     this.categories = [
       "HOME",
       "Headers",
@@ -23,6 +24,10 @@ class Categories extends Component {
       "Highlights",
       "Quote"
     ];
+
+    this.state = {
+      active: 0
+    };
   }
 
   render() {
@@ -34,9 +39,15 @@ class Categories extends Component {
           <h1 className="sgcreator-categories_header ">{name}</h1>
         </Link>
         <nav className="sgcreator-categories_list">
-          <i className="fas fa-arrow-right sgcreator-categories-list_arrow sgcreator-categories-list_arrow_right" />
+          <i
+            className="fas fa-arrow-left sgcreator-categories-list_arrow sgcreator-categories-list_arrow_left"
+            onClick={this.changeActive.bind(this, -1)}
+          />
           {this.renderIndexList()}
-          <i className="fas fa-arrow-left sgcreator-categories-list_arrow sgcreator-categories-list_arrow_left" />
+          <i
+            className="fas fa-arrow-right sgcreator-categories-list_arrow sgcreator-categories-list_arrow_right"
+            onClick={this.changeActive.bind(this, +1)}
+          />
         </nav>
         <Download data={this.props.data} />
       </section>
@@ -46,7 +57,9 @@ class Categories extends Component {
     return this.categories.map((category, idx) => {
       return (
         <Link
-          className="sgcreator-categories-list_item"
+          className={`sgcreator-categories-list_item ${
+            this.state.active === idx ? "enabled" : "disabled"
+          }`}
           to={this.formatUrl(category)}
           key={category}
         >
@@ -61,6 +74,20 @@ class Categories extends Component {
       .toLowerCase()
       .replace(" ", "-");
     return result === "home" ? "" : result;
+  }
+
+  changeActive(num, ev) {
+    let newIdx = this.state.active + num;
+    if (newIdx < 0) {
+      newIdx = this.categories.length - 1;
+    }
+    if (newIdx >= this.categories.length) {
+      newIdx = 0;
+    }
+
+    this.setState({
+      active: newIdx
+    });
   }
 }
 
